4e90066a6a77d99b3abdfafe59a64cbf85e9500c
bhavana-netlify <bhavana@netlify.com>
2023-02-14 14:39:33 -0500

    adding custom cache headers

---
+++ netlify/edge-functions/cache.ts
@@ -0,0 +1,9 @@
+
+
+export default async (Context) => {
+  const response = await Context.next();
+  response.headers.set("Cache-Control", "private, max-age=0, must-revalidate");
+  return response;
+}
+
+export const config = { path: "/test" }

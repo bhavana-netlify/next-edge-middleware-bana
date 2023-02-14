70b0c6ad8b9bf39ed553809ed6a0e5ee154f6cf3
bhavana-netlify <bhavana@netlify.com>
2023-02-14 14:52:30 -0500

    adding console log messages

--- netlify/edge-functions/cache.ts
+++ netlify/edge-functions/cache.ts
@@ -1,9 +1,13 @@
 
 
-export default async (Context) => {
+export default async (request: Request, Context) => {
+  const url = new URL(request.url);
   const response = await Context.next();
   response.headers.set("Cache-Control", "private, max-age=0, must-revalidate");
+  console.log(`Adding a custom header to the response for ${url}`);
   return response;
 }
+
+
 
 export const config = { path: "/" }

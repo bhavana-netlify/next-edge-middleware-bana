aefbe18e84a70b9c3bbec2b7578651b0eb9121d5
bhavana-netlify <42786844+bhavana-netlify@users.noreply.github.com>
2023-02-15 12:00:32 -0500

    Update cache.ts

--- netlify/edge-functions/cache.ts
+++ netlify/edge-functions/cache.ts
@@ -3,6 +3,8 @@ export default async (request: Request, Context) => {
   const url = new URL(request.url);
   const response = await Context.next();
   console.log('Header from SSR:', response.headers.get('cache-control'))
+  console.log(`Adding a custom header to the response for ${url}`);
   response.headers.set("Cache-Control", "private, max-age=0, must-revalidate")
+  console.log('Header from EF:', response.headers.get('cache-control'))
   return response;
 }
